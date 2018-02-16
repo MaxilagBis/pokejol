2f7e7f2e2f6ef98bd75bfac867f0c5679e153055
Maxilag <guillaumeseyer@hotmail.fr>
2018-02-16 11:29:23 +0100

    ajout de bootstrap

--- index.php
+++ index.php
@@ -1,45 +1,36 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <title>pokejol</title>
         <meta charset="utf-8" />
-        <title>Mon super site</title>
     </head>
- 
     <body>
- 
-    <!-- L'en-tête -->
-    
-    <header>
-       
-    </header>
-    
-    <!-- Le menu -->
-    
-    <?php include("menu.php"); ?>
-    
-    <!-- Le corps -->
-    <a href="bonjour.php?nom=Dupont&prenom=Jean&repeter=8">Dis-moi bonjour !</a>
-    <div id="corps">
-        <h1>Mon super site</h1>
+		<header>
+			<nav>
+			<ul>
+				<li><a href="index.html">Accueil</a></li>
+				<li><a href="forum.html">Forum</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</nav>
+		</header>
+        <h2>Page de test</h2>
+        
         <p>
-            Bienvenue sur mon super site !<br />
-            Vous allez adorer ici, c'est un site génial qui va parler de... euh... Je cherche encore un peu le thème de mon site. :-D
-			
-			<form method="post" action="cible.php">
- 
-			<p>prenom
-				<input type="text" name="prenom" />
-				<input type="submit" value="Valider" />
-			</p>
-		</form>
+            Cette page contient du code HTML avec des balises PHP.<br />
+            <?php /* Insérer du code PHP ici */ ?>
+            Voici quelques petits tests :
         </p>
-    </div>
-    
-    <!-- Le pied de page -->
-    
-    <footer id="pied_de_page">
-        <p>Copyright moi, tous droits réservés</p>
-    </footer>
-    
+        
+        <ul>
+        <li style="color: blue;">Texte en bleu</li>
+        <li style="color: red;">Texte en rouge</li>
+        <li style="color: green;">Texte en vert</li>
+        </ul>
+        
+        <?php
+        /* Encore du PHP
+        Toujours du PHP */
+        ?>
     </body>
 </html>
