eb4bb9286d865b5956d47b8f4b5e2182120de171
Naopil <naopil88@gmail.com>
2018-02-28 21:52:50 +0100

    Tests commit

--- index.php
+++ index.php
@@ -1,38 +1,42 @@
 <!DOCTYPE html>
 <html>
-    <head>
-		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/tuto.css" rel="stylesheet">
-		<script src="bootstrap/js/jquery.js"></script>
-		<script src="bootstrap/js/bootstrap.min.js"></script>
-        <title>pokejol</title>
-        <meta charset="utf-8" />
-    </head>
-   <body>
-    <div class="container">
 
-      <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-          <ul class="nav navbar-nav">
-            <li class="active"> <a href="#">Accueil</a> </li>
-            <li> <a href="#">Liens</a> </li>
-            <li> <a href="#">Témoignages</a> </li>
-            <li> <a href="#">Références</a> </li>
-          </ul>
-          <form class="navbar-form navbar-right inline-form">
-            <div class="form-group">
-              <input type="search" class="input-sm form-control" placeholder="Recherche">
-              <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
-            </div>
-          </form>
-        </div>
-      </nav>
-      
-		<div class="container-fluid">
-			<img class="img-responsive" src="img/kyogre.jpg" alt="">
-		</div>
-      
+<head>
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/tuto.css" rel="stylesheet">
+  <script src="bootstrap/js/jquery.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <title>pokejol</title>
+  <meta charset="utf-8" />
+</head>
+
+<body>
+  <div class="container">
+
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <ul class="nav navbar-nav">
+          <li class="active"> <a href="#">Accueil</a> </li>
+          <li> <a href="#">Liens</a> </li>
+          <li> <a href="#">Témoignages</a> </li>
+          <li> <a href="#">Références</a> </li>
+        </ul>
+        <form class="navbar-form navbar-right inline-form">
+          <div class="form-group">
+            <input type="search" class="input-sm form-control" placeholder="Recherche">
+            <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
+          </div>
+        </form>
+      </div>
+    </nav>
+
+    <div class="container-fluid">
+      <img class="img-responsive" src="img/kyogre.jpg" alt="">
     </div>
-  </body>
+
+  </div>
+</body>
+
 </html>
+
 </html>
