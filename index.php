1685f133de7d899e6cbf0d63fff1dd19ade6fcb0
Maxilag <guillaumeseyer@hotmail.fr>
2018-06-25 13:54:46 +0200

    rajout de l'équipe

--- index.php
+++ index.php
@@ -1,38 +1,56 @@
 <!DOCTYPE html>
 <html>
-    <head>
+    <head>	
 		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/tuto.css" rel="stylesheet">
+		<link href="css/Style.css" rel="stylesheet">
 		<script src="bootstrap/js/jquery.js"></script>
 		<script src="bootstrap/js/bootstrap.min.js"></script>
         <title>pokejol</title>
         <meta charset="utf-8" />
     </head>
    <body>
-    <div class="container">
+    <div class="container-fluid">
 
-      <nav class="navbar navbar-inverse">
-        <div class="container-fluid">
-          <ul class="nav navbar-nav">
-            <li class="active"> <a href="#">Accueil</a> </li>
-            <li> <a href="#">Liens</a> </li>
-            <li> <a href="#">Témoignages</a> </li>
-            <li> <a href="#">Références</a> </li>
-          </ul>
-          <form class="navbar-form navbar-right inline-form">
-            <div class="form-group">
-              <input type="search" class="input-sm form-control" placeholder="Recherche">
-              <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
-            </div>
-          </form>
-        </div>
-      </nav>
-      
-		<div class="container-fluid">
-			<img class="img-responsive" src="img/kyogre.jpg" alt="">
+    <?php include("menu.php"); ?>
+		
+	<div class="col-xs-12 col-sm-12 col-lg-12">
+			<h2 class="text-center"><strong>Concevez votre équipe JOL !<strong></h2>
+	</div>
+	<div class="collapse in">
+		<div class="col-xs-6 col-sm-4 col-lg-4">
+			<a href="">
+				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+			</a>
 		</div>
+		<div class="col-xs-6 col-sm-4 col-lg-4">
+			<a href="">
+				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+			</a>
+		</div>
+		<div class="col-xs-6 col-sm-4 col-lg-4">
+			<a href="">
+				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+			</a>
+		</div>
+	</div>
+	<div class="collapse in">
+		<div class="col-xs-6 col-sm-4 col-lg-4">
+			<a href="">
+				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+			</a>
+		</div>
+		<div class="col-xs-6 col-sm-4 col-lg-4 center-block">
+			<a href="">
+				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+			</a>
+		</div>
+		<div class="col-xs-6 col-sm-4 col-lg-4 center-block">
+			<a href="">
+				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+			</a>
+		</div>
+	</div>
       
     </div>
   </body>
-</html>
 </html>
--- menu.php
+++ menu.php
@@ -1,10 +1,24 @@
-<nav id="menu">        
-    <div class="element_menu">
-        <h3>Titre menu</h3>
-        <ul>
-            <li><a href="page1.html">Lien</a></li>
-            <li><a href="page2.html">Lien</a></li>
-            <li><a href="page3.html">Lien</a></li>
-        </ul>
-    </div>    
+
+<div class="row">
+<nav class="navbar-inverse">
+	<div class="container-fluid">
+		<ul class="nav navbar-nav">
+			<li class="active"> <a href="#">Accueil</a> </li>
+			<li> <a href="#">Couverture</a> </li>
+			<li> <a href="#">Règles</a> </li>
+			<li> <a href="#">A propos</a> </li>
+		</ul>
+		<form class="navbar-form navbar-right inline-form">
+			<div class="form-group">
+				<input type="search" class="input-sm form-control" placeholder="Recherche">
+				<button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
+			</div>
+        </form>
+	</div>
 </nav>
+<div class="mon-image">
+	<img class="img-responsive" src="img/banniere.png">
+</div>
+</div>
+      
+
