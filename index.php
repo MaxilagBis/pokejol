adf96ea3cc784e767051d3b9c64f095e75f00bd1
Naopil <naopil88@gmail.com>
2018-07-15 23:37:15 +0200

    Beautify
    Désolé, j'aime bien le code rangé à la tabulation près Kappa

--- index.php
+++ index.php
@@ -1,56 +1,64 @@
 <!DOCTYPE html>
 <html>
-    <head>	
-		<link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
-		<link href="css/Style.css" rel="stylesheet">
-		<script src="bootstrap/js/jquery.js"></script>
-		<script src="bootstrap/js/bootstrap.min.js"></script>
-        <title>pokejol</title>
-        <meta charset="utf-8" />
-    </head>
-   <body>
-    <div class="container-fluid">
+
+<head>
+  <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/Style.css" rel="stylesheet">
+  <script src="bootstrap/js/jquery.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <title>pokejol</title>
+  <meta charset="utf-8" />
+</head>
+
+<body>
+  <div class="container-fluid">
 
     <?php include("menu.php"); ?>
-		
-	<div class="col-xs-12 col-sm-12 col-lg-12">
-			<h2 class="text-center"><strong>Concevez votre équipe JOL !<strong></h2>
-	</div>
-	<div class="collapse in">
-		<div class="col-xs-6 col-sm-4 col-lg-4">
-			<a href="">
-				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
-			</a>
-		</div>
-		<div class="col-xs-6 col-sm-4 col-lg-4">
-			<a href="">
-				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
-			</a>
-		</div>
-		<div class="col-xs-6 col-sm-4 col-lg-4">
-			<a href="">
-				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
-			</a>
-		</div>
-	</div>
-	<div class="collapse in">
-		<div class="col-xs-6 col-sm-4 col-lg-4">
-			<a href="">
-				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
-			</a>
-		</div>
-		<div class="col-xs-6 col-sm-4 col-lg-4 center-block">
-			<a href="">
-				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
-			</a>
-		</div>
-		<div class="col-xs-6 col-sm-4 col-lg-4 center-block">
-			<a href="">
-				<img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
-			</a>
-		</div>
-	</div>
-      
+
+    <div class="col-xs-12 col-sm-12 col-lg-12">
+      <h2 class="text-center"><strong>Concevez votre équipe JOL !</strong></h2>
     </div>
-  </body>
+
+    <div class="collapse in">
+      <div class="col-xs-6 col-sm-4 col-lg-4">
+        <a href="">
+          <img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+        </a>
+      </div>
+
+      <div class="col-xs-6 col-sm-4 col-lg-4">
+        <a href="">
+          <img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+        </a>
+      </div>
+
+      <div class="col-xs-6 col-sm-4 col-lg-4">
+        <a href="">
+          <img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+        </a>
+      </div>
+    </div>
+
+    <div class="collapse in">
+      <div class="col-xs-6 col-sm-4 col-lg-4">
+        <a href="">
+          <img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+        </a>
+      </div>
+
+      <div class="col-xs-6 col-sm-4 col-lg-4 center-block">
+        <a href="">
+          <img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+        </a>
+      </div>
+
+      <div class="col-xs-6 col-sm-4 col-lg-4 center-block">
+        <a href="">
+          <img class="center-block img-pokeball img-responsive" src="img/pokeball.jpg" alt="Ajouter un pokemon">
+        </a>
+      </div>
+    </div>
+  </div>
+</body>
+
 </html>
--- menu.php
+++ menu.php
@@ -1,24 +1,23 @@
-
 <div class="row">
-<nav class="navbar-inverse">
-	<div class="container-fluid">
-		<ul class="nav navbar-nav">
-			<li class="active"> <a href="#">Accueil</a> </li>
-			<li> <a href="#">Couverture</a> </li>
-			<li> <a href="#">Règles</a> </li>
-			<li> <a href="#">A propos</a> </li>
-		</ul>
-		<form class="navbar-form navbar-right inline-form">
-			<div class="form-group">
-				<input type="search" class="input-sm form-control" placeholder="Recherche">
-				<button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
-			</div>
-        </form>
-	</div>
-</nav>
-<div class="mon-image">
-	<img class="img-responsive" src="img/banniere.png">
-</div>
-</div>
-      
+  <nav class="navbar-inverse">
+    <div class="container-fluid">
+      <ul class="nav navbar-nav">
+        <li class="active"> <a href="#">Accueil</a> </li>
+        <li> <a href="#">Couverture</a> </li>
+        <li> <a href="#">Règles</a> </li>
+        <li> <a href="#">À propos</a> </li>
+      </ul>
 
+      <form class="navbar-form navbar-right inline-form">
+        <div class="form-group">
+          <input type="search" class="input-sm form-control" placeholder="Recherche">
+          <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
+        </div>
+      </form>
+    </div>
+  </nav>
+
+  <div class="mon-image">
+    <img class="img-responsive" src="img/banniere.png">
+  </div>
+</div>
